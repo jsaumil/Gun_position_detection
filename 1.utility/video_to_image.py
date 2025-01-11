447b08e45467a37820bda63e63357814c162e3e7
Saumil Jain <156582131+jsaumil@users.noreply.github.com>
2025-01-11 22:48:40 +0530

    Updated all files

--- 1.utility/video_to_image.py
+++ 1.utility/video_to_image.py
@@ -46,6 +46,7 @@ else:
             cv2.imwrite(frame_filename, frame)
             print(f"Saved: {frame_filename}")
             frame_count += 1
+            
             # Save frames at 1 FPS
             # if frame_index % frame_skip == 0:
             #     frame_filename = os.path.join(output_dir, f"frame_{frame_count:04d}.jpg")
