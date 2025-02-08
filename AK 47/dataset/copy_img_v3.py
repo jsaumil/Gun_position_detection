6767bbf912f1f8c2cf485e6bec693d47722b2e05
Saumil Jain <156582131+jsaumil@users.noreply.github.com>
2025-02-08 18:40:17 +0530

    Added image frame_0920.jpg

--- AK 47/dataset/copy_img_v3.py
+++ AK 47/dataset/copy_img_v3.py
@@ -21,7 +21,7 @@ def git_push(commit_message="Added new images batch"):
         print(f"Git error: {e}")
 
 if __name__ == "__main__":
-    source_folder = r"E:\Github\New folder (2)\data\photos\AK 47\V4"  # Update this path
+    source_folder = r"E:\Github\New folder (2)\data\photos\AK 47\V3"  # Update this path
     destination_folder = r"E:\Github\Gun_position_detection\AK 47\dataset\photos\V4"  # Update this path
     
     images = sorted(os.listdir(source_folder))
