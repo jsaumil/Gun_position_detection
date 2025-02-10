ddff6b35ff48618243ab8f9857dc22c30c1d974c
Saumil Jain <156582131+jsaumil@users.noreply.github.com>
2025-02-10 14:07:26 +0530

    Added batch 1 of images

--- AK 47/dataset/copy_img_v3.py
+++ AK 47/dataset/copy_img_v3.py
@@ -11,29 +11,30 @@ def copy_images(image_list, source_folder, destination_folder):
         shutil.copy(source_image, destination_folder)
         print(f"Image copied: {image}")
 
-def git_push(commit_message="Added new images batch"):
+def git_commit(commit_message="Added new images batch"):
     try:
         subprocess.run(["git", "add", "*"], check=True)
         subprocess.run(["git", "commit", "-m", commit_message], check=True)
+        print("Changes committed to Git repository.")
+    except subprocess.CalledProcessError as e:
+        print(f"Git commit error: {e}")
+
+def git_push():
+    try:
         subprocess.run(["git", "push"], check=True)
         print("Changes pushed to Git repository.")
     except subprocess.CalledProcessError as e:
-        print(f"Git error: {e}")
+        print(f"Git push error: {e}")
 
 if __name__ == "__main__":
     source_folder = r"E:\Github\New folder (2)\data\photos\AK 47\V3"  # Update this path
     destination_folder = r"E:\Github\Gun_position_detection\AK 47\dataset\photos\V3"  # Update this path
     
     images = sorted(os.listdir(source_folder))
-    mid_index = len(images) // 2
+    batch_size = 500
     
-    first_half = images[:mid_index]
-    second_half = images[mid_index:]
-    
-    # Process first half
-    copy_images(first_half, source_folder, destination_folder)
-    git_push(commit_message="Added first half of images")
-    
-    # Process second half
-    copy_images(second_half, source_folder, destination_folder)
-    git_push(commit_message="Added second half of images")
+    for i in range(0, len(images), batch_size):
+        batch = images[i:i+batch_size]
+        copy_images(batch, source_folder, destination_folder)
+        git_commit(commit_message=f"Added batch {i//batch_size + 1} of images")
+        git_push()
